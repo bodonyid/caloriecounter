ef71692b724919df345b12d667ebb7ca3f8d5629
Daniel Bodonyi <bodonyid@gmail.com>
2016-01-26 10:22:04 +0100

    Add List Meals functionality in front-end JS with basic table styling

--- public/index.js
+++ public/index.js
@@ -5,6 +5,10 @@ var mealInputBox = document.querySelector('.meal-input');
 var calorieInputBox = document.querySelector('.calorie-input');
 var dateInputBox = document.querySelector('.date-input');
 var addMealButton = document.querySelector('.add-meal-button');
+var listMealsButton = document.querySelector('.list-meals-button');
+var mealListNameColumn = document.querySelector('.meal-list-name');
+var mealListCaloriesColumn = document.querySelector('.meal-list-calories');
+var mealListDateColumn = document.querySelector('.meal-list-date');
 
 function createRequest(method, url, data, callback) {
   var newRequest = new XMLHttpRequest();
@@ -36,4 +40,31 @@ function createPostRequest() {
   createRequest('POST', url, newMeal, refresh);
 }
 
+function createGetAllRequest() {
+  createRequest('GET', url, {}, listMeals);
+}
+
+var listMeals = function(response) {
+  var meals = JSON.parse(response);
+  mealListNameColumn.innerText = 'Meal';
+  mealListCaloriesColumn.innerText = 'Calories';
+  mealListDateColumn.innerText = 'Date';
+  meals.forEach(function(meal) {
+    var newMealItemName = document.createElement('div');
+    newMealItemName.classList.add('meal-list-item-name');
+    newMealItemName.innerText = meal.name;
+    mealListNameColumn.appendChild(newMealItemName);
+    var newMealItemCalories = document.createElement('div');
+    newMealItemCalories.classList.add('meal-list-item-calories');
+    newMealItemCalories.innerText = meal.calories;
+    mealListCaloriesColumn.appendChild(newMealItemCalories);
+    var newMealItemDate = document.createElement('div');
+    newMealItemDate.classList.add('meal-list-item-date');
+    newMealItemDate.innerText = meal.date;
+    mealListDateColumn.appendChild(newMealItemDate);
+  });
+}
+
 addMealButton.addEventListener('click', createPostRequest);
+
+listMealsButton.addEventListener('click', createGetAllRequest);
